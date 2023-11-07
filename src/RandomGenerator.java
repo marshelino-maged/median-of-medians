5452664161e8a713667fdf16fdac719d841b39ae
marshelino-maged <maromaged169@gmail.com>
2023-11-07 05:54:54 +0200

    implementation of the random generator

---
+++ src/RandomGenerator.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomGenerator {
+    public static int[] generateInts(int n){
+        HashSet<Integer> hash = HashSet.newHashSet(n);
+        int[] arr = new int[n];
+        int i=0;
+        while(i<n){
+            Integer num = ThreadLocalRandom.current().nextInt(-10000000, 10000000);
+            if(!hash.contains(num)){
+                hash.add(num);
+                arr[i] = num;
+                i++;
+            }
+        }
+        return arr;
+    }
+}
