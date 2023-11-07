0312a32e40632d8f7da766ac8b4b29b96b4e1598
marshelino-maged <maromaged169@gmail.com>
2023-11-07 05:55:17 +0200

    add time comparison to the test cases

--- src/test.java
+++ src/test.java
@@ -1,4 +1,8 @@
 import org.junit.Test;
+
+import java.sql.Time;
+import java.util.Timer;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -31,6 +35,70 @@ public class test {
         int m2 = Methods.random_selection(arr);
 //        int m3 = Methods.median_of_medians(arr);
         assertEquals(m1,m2);
+
+    }
+
+    @Test
+    public void t_100(){
+        int[] arr = RandomGenerator.generateInts(100);
+        int m1 = Methods.sort_and_return(arr);
+        int m2 = Methods.random_selection(arr);
+//        int m3 = Methods.median_of_medians(arr);
+        assertEquals(m1,m2);
+
+    }
+
+    @Test
+    public void t_1000(){
+        int[] arr = RandomGenerator.generateInts(1000);
+        int m1 = Methods.sort_and_return(arr);
+        int m2 = Methods.random_selection(arr);
+//        int m3 = Methods.median_of_medians(arr);
+        assertEquals(m1,m2);
+
+    }
+
+    @Test
+    public void t_10000(){
+        int[] arr = RandomGenerator.generateInts(10000);
+        int m1 = Methods.sort_and_return(arr);
+        int m2 = Methods.random_selection(arr);
+//        int m3 = Methods.median_of_medians(arr);
+        assertEquals(m1,m2);
+
+    }
+
+    @Test
+    public void t_100000(){
+        int[] arr = RandomGenerator.generateInts(100000);
+        int m1 = Methods.sort_and_return(arr);
+        int m2 = Methods.random_selection(arr);
+//        int m3 = Methods.median_of_medians(arr);
+        assertEquals(m1,m2);
+
+    }
+
+    @Test
+    public void t_1000000(){
+        int[] arr = RandomGenerator.generateInts(1000000);
+
+        long before = System.currentTimeMillis();
+        int m1 = Methods.sort_and_return(arr);
+        long after = System.currentTimeMillis();
+
+        long t1 = after-before;
+
+        before = System.currentTimeMillis();
+        int m2 = Methods.random_selection(arr);
+        after = System.currentTimeMillis();
+        long t2 = after-before;
+
+//        int m3 = Methods.median_of_medians(arr);
+        assertEquals(m1,m2);
+
+
+        System.out.println("sort all time: "+ t1 );
+        System.out.println("random select sort time: " + t2);
 
     }
 }
